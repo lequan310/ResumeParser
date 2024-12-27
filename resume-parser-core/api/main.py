dbe374b71608b77d900042f216fed2a8a0709dcb
Quan Le <leminhquan310@gmail.com>
2024-12-27 23:18:18 +0700

    refactor: modify env variables

--- resume-parser-core/api/main.py
+++ resume-parser-core/api/main.py
@@ -6,7 +6,11 @@ load_dotenv()
 
 
 def main():
-    uvicorn.run("api.app:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(
+        "api.app:app",
+        host=os.getenv("API_HOST", "0.0.0.0"),
+        port=int(os.getenv("API_PORT", 8000)),
+    )
 
 
 if __name__ == "__main__":
