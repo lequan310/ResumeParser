8e5e794d8c7831a5d53cd66db5d45b0ee8b1e075
Quan Le <leminhquan310@gmail.com>
2025-02-05 15:33:35 +0700

    bugfix: update env key

--- webapp/src/services/axiosInstance.ts
+++ webapp/src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.PARSER_API || "http://localhost:8000",
+    baseURL: import.meta.env.VITE_PARSER_API || "http://localhost:8000",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
