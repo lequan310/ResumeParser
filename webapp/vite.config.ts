d22b6f9ad0df9e78c3a8fe08cbd7e1cfbf135b94
Quan Le <leminhquan310@gmail.com>
2025-02-05 14:59:05 +0700

    feat: add error handling for routing vercel

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,18 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-// import path from "path"; // Import path
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//     plugins: [react(), tailwindcss()],
-//     resolve: {
-//         alias: {
-//             "@": path.resolve(__dirname, "./src"), // This is the key part
-//         },
-//     },
-// });
-
 import path from "node:path";
 import { createRequire } from "node:module";
 import { defineConfig, normalizePath } from "vite";
