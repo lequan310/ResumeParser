e81126a0d2987371f3fd3861ae8285a3d1a118cd
Quan Le <leminhquan310@gmail.com>
2025-11-04 19:13:48 +0700

    fix lint

--- resume-parser-core/core/workflows/parser/utils/nodes.py
+++ resume-parser-core/core/workflows/parser/utils/nodes.py
@@ -1,10 +1,10 @@
+from core.workflows.models.resume import Resume
 from core.workflows.parser.utils.parse_utils import (
     convert_markdown_to_resume,
     convert_to_markdown,
 )
 from core.workflows.parser.utils.state import InputState, State
 from core.workflows.parser.utils.tools import get_position_duration
-from core.workflows.models.resume import Resume
 
 
 async def get_resume_markdown(state: InputState):
